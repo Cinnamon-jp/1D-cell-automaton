51553b4da82d3d4557de5679d78a52849fb9b3d5
Cinnamon-jp <cinnamon312.dev@gmail.com>
2026-06-30 15:11:53 +0900

    100ビットの更新処理を作成

--- main.go
+++ main.go
@@ -25,3 +25,34 @@ func randomBits(r *rand.Rand) (bits []byte) {
 	return
 }
 
+// updateBits は、アップデートルールに従ってビット列を更新する関数
+func updateBits(oldBits []byte, rule byte) (newBits []byte, err error) {
+	newBits = make([]byte, 100)
+	var target3Bits byte
+
+	// ビット列の長さチェック
+	if len(oldBits) != 100 {
+		return nil, errors.New("length of oldBits must be 100")
+	}
+	
+	for i, _ := range oldBits {
+		if i != 0 {
+			target3Bits = oldBits[i-1]
+		} else {
+			target3Bits = oldBits[99]
+		}
+		
+		target3Bits = target3Bits << 1 | oldBits[i]
+
+		if i != 99 {
+			target3Bits = target3Bits << 1 | oldBits[i+1]
+		} else {
+			target3Bits = target3Bits << 1 | oldBits[0]
+		}
+
+		// ruleの中から target3Bits に対応するビットを取り出し、次の状態とする
+		newBits[i] = (rule >> target3Bits) & 1
+	}
+
+	return newBits, nil
+}
