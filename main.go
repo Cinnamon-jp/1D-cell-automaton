ca36311b31d263902bd685632b40f6edd64a13dd
Cinnamon-jp <cinnamon312.dev@gmail.com>
2026-07-01 22:41:38 +0900

    getVal()をutil.goに移動

--- main.go
+++ main.go
@@ -72,42 +72,6 @@ func run() error {
 			}
 		}
 	}
-
-	return nil
 }
 
-// getVal は、min から max までの範囲で val を取得する関数
-func getVal(prompt string, min int, max int, defVal int) (int, error) {
-	scanner := bufio.NewScanner(os.Stdin)
 
-	for {
-		fmt.Print(prompt)
-		if !scanner.Scan() {
-			if err := scanner.Err(); err != nil {
-				return -1, err
-			}
-			return -1, errors.New("入力が途絶えました") // EOF
-		}
-
-		input := strings.TrimSpace(scanner.Text())
-
-		if input == "" {
-			if defVal == -1 {
-				fmt.Println("入力が必要です")
-				continue
-			}
-			return defVal, nil
-		}
-
-		val, err := strToInt(input)
-		if err != nil {
-			fmt.Println("入力が数値ではありません")
-			continue
-		}
-		if val < min || val > max {
-			fmt.Printf("入力が範囲外です min: %d, max: %d\n", min, max)
-			continue
-		}
-		return val, nil
-	}
-}
--- util.go
+++ util.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func scanInput(prompt string) (string, error) {
@@ -18,4 +22,40 @@ func scanInput(prompt string) (string, error) {
 
 func strToInt(s string) (int, error) {
 	return strconv.Atoi(s)
+}
+
+// getVal は、min から max までの範囲で val を取得する関数
+func getVal(prompt string, min int, max int, defVal int) (int, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				return -1, err
+			}
+			return -1, errors.New("入力が途絶えました") // EOF
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+
+		if input == "" {
+			if defVal == -1 {
+				fmt.Println("入力が必要です")
+				continue
+			}
+			return defVal, nil
+		}
+
+		val, err := strToInt(input)
+		if err != nil {
+			fmt.Println("入力が数値ではありません")
+			continue
+		}
+		if val < min || val > max {
+			fmt.Printf("入力が範囲外です min: %d, max: %d\n", min, max)
+			continue
+		}
+		return val, nil
+	}
 }
